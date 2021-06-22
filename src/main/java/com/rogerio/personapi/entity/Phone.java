2586a79f639197cd041dfa3d275a9fe40d686335
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-22 15:25:51 -0300

    mapping to JPA entities person and phones

--- src/main/java/com/rogerio/personapi/entity/Phone.java
+++ src/main/java/com/rogerio/personapi/entity/Phone.java
@@ -2,19 +2,26 @@ package com.rogerio.personapi.entity;
 
 import com.rogerio.personapi.enums.PhoneType;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
+
+    @Column(nullable = false)
     private String number;
 
 }
