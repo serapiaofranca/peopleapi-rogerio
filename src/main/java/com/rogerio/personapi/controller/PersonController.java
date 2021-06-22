3b6300addd108294292880388fddabbac740fb6a
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-22 16:40:57 -0300

    created person_service, and refactory person_controller

--- src/main/java/com/rogerio/personapi/controller/PersonController.java
+++ src/main/java/com/rogerio/personapi/controller/PersonController.java
@@ -2,27 +2,26 @@ package com.rogerio.personapi.controller;
 
 import com.rogerio.personapi.dto.response.MessageResponseDTO;
 import com.rogerio.personapi.entity.Person;
-import com.rogerio.personapi.repository.PersonRepository;
+import com.rogerio.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO.builder()
-                .message("Created person with ID: " + savedPerson.getId())
-                .build();
+       return personService.createPerson(person);
     }
+
 }
