bc514ccf8c522e09cb4b5372a1908fa16d1bb43d
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-13 20:52:11 -0300

    Using lombok_AllArgsContructor, created Readme to project

--- src/main/java/com/rogerio/personapi/controller/PersonController.java
+++ src/main/java/com/rogerio/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.rogerio.personapi.dto.response.MessageResponseDTO;
 import com.rogerio.personapi.entity.Person;
 import com.rogerio.personapi.exception.PersonNotFoundException;
 import com.rogerio.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/rogerio/personapi/service/PersonService.java
+++ src/main/java/com/rogerio/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.rogerio.personapi.entity.Person;
 import com.rogerio.personapi.exception.PersonNotFoundException;
 import com.rogerio.personapi.mapper.PersonMapper;
 import com.rogerio.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,12 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
