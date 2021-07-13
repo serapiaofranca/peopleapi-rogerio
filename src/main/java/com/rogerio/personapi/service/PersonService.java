d09ce37c705c7c400486285d9496aad9588d7813
Rogerio A Serapiao <rserapa@gmail.com>
2021-07-13 16:53:51 -0300

    implemented all_users_list

--- src/main/java/com/rogerio/personapi/service/PersonService.java
+++ src/main/java/com/rogerio/personapi/service/PersonService.java
@@ -7,7 +7,9 @@ import com.rogerio.personapi.mapper.PersonMapper;
 import com.rogerio.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -28,5 +30,11 @@ public class PersonService {
         return MessageResponseDTO.builder()
                 .message("Created person with ID: " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
